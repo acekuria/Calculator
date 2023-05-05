5f66fdf488232a10cd8d4b2c21d9aeea4af77e79
AlvinKuria <alvinkuria18@gmail.com>
2023-05-05 19:48:55 +0100

    JavaScript functions for calculations are done.
    Created add, subtract, multiply and divide functions.
    Created operate function which uses the other functions depending on what operator the user has chosen

---
+++ script.js
@@ -0,0 +1,65 @@
+function roundToTwo(num) {
+  return +(Math.round(num + "e+2")  + "e-2");
+}
+
+function add (...args) {
+  let total = 0
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return roundToTwo(total)
+}
+
+function subtract (...args) {
+  let total = args[0]
+  for (let i = 0; i < args.length; i++) {
+    total -= args[i];
+  }
+  return roundToTwo(total)
+}
+
+function multiply (...args) {
+  let total = 1
+  for (let i = 0; i < args.length; i++) {
+    total *= args[i];
+  }
+  return roundToTwo(total) 
+}
+
+function divide (...args) {
+  let total = args[0]
+  for (let i = 1; i < args.length; i++) {
+    total /= args[i];
+  }
+  return roundToTwo(total) 
+}
+
+let num1 = document.querySelector('.num1')
+let operator = document.querySelector('.operator')
+let num2 = document.querySelector('.num2')
+
+
+function operate (num1, operator, num2) {
+  let operator = ['+', '-', 'x', '/']
+  if (operator[0]) {
+    return add(num1, num2)
+  }
+  if (operator[1]) {
+    return subtract(num1, num2)
+  }
+  if (operator[2]) {
+    return multiply(num1, num2)
+  }
+  if (operator[3]) {
+    return divide(num1, num2)
+  }
+}
+
+
+
+
+
+console.log(add(5.93284824,8,2,3,6));
+console.log(subtract(5,8,2,3,6));
+console.log(divide(5,10));
+console.log(multiply(5,8));
